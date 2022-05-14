6498495526431e2b93320365286d930a305dea01
coconan <coconan8@gmail.com>
2022-05-14 11:04:53 +0800

    Initial Commit

---
+++ ios-hello-world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-hello-world
+//
+//  Created by brian.yang on 2022/5/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-hello-world/ios_hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_hello_worldApp.swift
+//  ios-hello-world
+//
+//  Created by brian.yang on 2022/5/14.
+//
+
+import SwiftUI
+
+@main
+struct ios_hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
