6aa9abbc3fc7ffcafac06020646988d2f4dcaeaf
coconan <coconan8@gmail.com>
2022-06-05 07:48:55 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by brian.yang on 2022/6/5.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
