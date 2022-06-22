218d81795e9ad3782db80c2926ad0b538c7b689f
coconan <coconan.by@gmail.com>
2022-06-23 07:45:30 +0800

    03. Layout Code and Preview

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -15,29 +15,39 @@ struct ContentView: View {
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
-                .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                .cornerRadius(20.0)
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
-            .fontWeight(.bold)
-            Text("20 sections - 3 hours")
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
+                .foregroundColor(.secondary)
             Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
         }
-        .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+        .padding(.all, 20.0)
+        .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
-        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
-        .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
-            
+        .background(Color("Background"))
+        .cornerRadius(30.0)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal, 20)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+                .environment(\.sizeCategory, .accessibilityLarge)
+                .previewDevice("iPhone 13 mini")
+        }
     }
 }
