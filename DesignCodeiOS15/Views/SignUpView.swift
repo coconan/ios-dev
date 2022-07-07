eed19323d61a767dcdf8e61a9e364e527771e80d
coconan <coconan.by@gmail.com>
2022-07-07 14:53:01 +0800

    47. Custom Input Style

--- DesignCodeiOS15/Views/SignUpView.swift
+++ DesignCodeiOS15/Views/SignUpView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             Text("Sign up")
                 .font(.largeTitle).bold()
             Text("Access 120+ hours of courses, tutorials and livestreams")
                 .font(.headline)
+            TextField("Email", text: $email)
+                .inputStyle(icon: "mail")
+                .textContentType(.emailAddress)
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+                .disableAutocorrection(true)
+            SecureField("Password", text: $password)
+                .inputStyle(icon: "lock")
+                .textContentType(.password)
             Button {} label: {
                 Text("Create an account")
                     .frame(maxWidth: .infinity)
