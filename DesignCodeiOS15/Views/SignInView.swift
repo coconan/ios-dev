ee322861f5ec8f5a6eb74c33f6275b024196f872
coconan <coconan.by@gmail.com>
2022-07-07 14:56:53 +0800

    50. Manage Multiple Modals

--- DesignCodeiOS15/Views/SignInView.swift
+++ DesignCodeiOS15/Views/SignInView.swift
@@ -1,13 +1,13 @@
 //
-//  SignUpView.swift
+//  SignInView.swift
 //  DesignCodeiOS15
 //
-//  Created by Meng To on 2022-02-17.
+//  Created by Meng To on 2022-02-23.
 //
 
 import SwiftUI
 
-struct SignUpView: View {
+struct SignInView: View {
     enum Field: Hashable {
         case email
         case password
@@ -24,7 +24,7 @@ struct SignUpView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
-            Text("Sign up")
+            Text("Sign in")
                 .font(.largeTitle).bold()
             Text("Access 120+ hours of courses, tutorials and livestreams")
                 .font(.headline)
@@ -51,7 +51,7 @@ struct SignUpView: View {
                     passwordY =  value
                 }
             Button {} label: {
-                Text("Create an account")
+                Text("Sign in")
                     .frame(maxWidth: .infinity)
             }
             .font(.headline)
@@ -61,18 +61,15 @@ struct SignUpView: View {
             .controlSize(.large)
             
             Group {
-                Text("By clicking on ")
-                + Text("_Create an account_").foregroundColor(.primary.opacity(0.7))
-                + Text(", you agree to our **Terms of Service** and **[Privacy Policy](https://designcode.io)**")
                 
                 Divider()
                 
                 HStack {
-                    Text("Already have an account?")
+                    Text("No account yet?")
                     Button {
-                        model.selectedModal = .signIn
+                        model.selectedModal = .signUp
                     } label: {
-                        Text("**Sign in**")
+                        Text("**Sign up**")
                     }
                 }
             }
@@ -115,11 +112,10 @@ struct SignUpView: View {
     }
 }
 
-struct SignUpView_Previews: PreviewProvider {
+struct SignInView_Previews: PreviewProvider {
     static var previews: some View {
         ZStack {
-            SignUpView()
-                .preferredColorScheme(.light)
+            SignInView()
                 .environmentObject(Model())
         }
     }
