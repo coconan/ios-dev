d024667758d977de06df2d03187d770b0bd4730c
coconan <coconan.by@gmail.com>
2022-07-07 15:04:26 +0800

    60. Dynamic Type Sizes

--- DesignCodeiOS15/Components/Items/FeaturedItem.swift
+++ DesignCodeiOS15/Components/Items/FeaturedItem.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct FeaturedItem: View {
     var course: Course = courses[0]
+    @Environment(\.sizeCategory) var sizeCategory
     
     var body: some View {
         VStack(alignment: .leading, spacing: 8.0) {
@@ -26,6 +27,7 @@ struct FeaturedItem: View {
                 .fontWeight(.bold)
                 .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
                 .lineLimit(1)
+                .dynamicTypeSize(.large)
             Text(course.subtitle.uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
@@ -33,7 +35,7 @@ struct FeaturedItem: View {
             Text(course.text)
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
-                .lineLimit(2)
+                .lineLimit(sizeCategory > .large ? 1 : 2)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .foregroundColor(.secondary)
         }
@@ -52,5 +54,6 @@ struct FeaturedItem: View {
 struct FeaturedItem_Previews: PreviewProvider {
     static var previews: some View {
         FeaturedItem()
+            .environment(\.sizeCategory, .extraExtraLarge)
     }
 }
